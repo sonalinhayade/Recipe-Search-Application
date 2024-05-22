40a23b53e6511e33d6c70250a584a71b17c772db
Sonali Nhayade <sonali.snhayade@gmail.com>
2024-05-22 14:58:09 +0530

    Update RecipeList.js

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -5,9 +5,9 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { MdHome } from "react-icons/md";
+import { Form } from "react-bootstrap";
 
-
-import "../App.css"; 
+import "../App.css";
 
 const RecipeList = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -38,64 +38,70 @@ const RecipeList = () => {
 
   return (
     <div className="dark-background">
-       <div className="home-button-container">
-        <Link to="/" className="home-button">
-        <MdHome className="home-icon"/>
-        </Link>
-      </div>
-      <div className="search-form-list">
-        <form onSubmit={handleSearch}>
+      <div className="header-list">
+        <div className="home-button-container">
+          <Link to="/" className="home-button">
+            <MdHome className="home-icon" />
+          </Link>
+        </div>
+
+        <Form className="search-form-list" onSubmit={handleSearch}>
           <input
             type="text"
-            className="search-input"
-            placeholder="Search by ingredient..."
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder="Search ingredients here..."
+            className="search-input-list"
           />
-          <button type="submit" className="search-list-button">
-          <FaSearch  className="search-ingredient-button"/>
-
+          <button
+            variant="outline-light"
+            className="search-button-list"
+            type="submit"
+          >
+            <FaSearch className="search-ingredient-list" />
           </button>
-        </form>
+        </Form>
       </div>
-    <div className="recipe-list-container">
-      {loading && <p>Loading...</p>}
-      {error && (
-        <div>
-          <p>Error: {error}</p>
-        </div>
-      )}
-      {!ingredient || !recipes ? (
-        <div className="no-recipe-found-container">
-          <h2>No Recipe Found</h2>
-          <p>No recipes found for the given ingredient. Please try another search.</p>
-        </div>
-      ) : (
-        <div className="recipe-card-container">
-          {recipes.map((recipe) => (
-            <div key={recipe.idMeal} className="recipe-list-card">
-              <img
-                src={recipe.strMealThumb}
-                alt={recipe.strMeal}
-                className="recipe-image"
-              />
-              <div className="recipe-details">
-                <h3 className="recipe-name">{recipe.strMeal}</h3>
-                <p className="recipe-description">
-                  {recipe.strDescription}
-                </p>
-                <button
-                  className="view-recipe-button"
-                  onClick={() => handleViewDetails(recipe.idMeal)}
-                >
-                  View Recipe
-                </button>
+
+      <div className="recipe-list-container">
+        {loading && <p>Loading...</p>}
+        {error && (
+          <div>
+            <p>Error: {error}</p>
+          </div>
+        )}
+        {!ingredient || !recipes ? (
+          <div className="no-recipe-found-container">
+            <h2>No Recipe Found</h2>
+            <p>
+              No recipes found for the given ingredient. Please try another
+              search.
+            </p>
+          </div>
+        ) : (
+          <div className="recipe-card-container">
+            {recipes.map((recipe) => (
+              <div key={recipe.idMeal} className="recipe-list-card">
+                <img
+                  src={recipe.strMealThumb}
+                  alt={recipe.strMeal}
+                  className="recipe-image"
+                />
+                <div className="recipe-details">
+                  <h3 className="recipe-name">{recipe.strMeal}</h3>
+                  <p className="recipe-description">{recipe.strDescription}</p>
+                  <button
+                    className="view-recipe-button"
+                    onClick={() => handleViewDetails(recipe.idMeal)}
+                  >
+                    View Recipe
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
